df5bb9ef1c491658bfa4fcc94c58096825f66fef
Parth Chavda <parthchavda.work@gmail.com>
2026-05-30 12:26:56 +0530

    fix: object to strict object

--- schema/userZodSchema.js
+++ schema/userZodSchema.js
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-export const signupZodSchema = z.object({
+export const signupZodSchema = z.strictObject({
     email: z.email("Invalid Email"),
     password: z.string().min(5, "Atleast 5 character required"),
     firstName: z.string(),
