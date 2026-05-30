cf6c11e9b80919d57f1b26a652eb31bd944a62ac
Parth Chavda <parthchavda.work@gmail.com>
2026-05-30 18:45:03 +0530

    feat: add article owner middleware

---
+++ middlewares/isOwner.js
@@ -0,0 +1,24 @@
+import { articleModel } from "../models/article.js";
+
+export async function isOwner(req, res, next) {
+    const articleId = req.params.id;
+    //find by id
+    const findArticle = await articleModel.findOne({
+        _id: articleId
+    });
+
+    if (!findArticle) {
+        return res.status(404).json({
+            msg: "Article Not found"
+        });
+    }
+
+    if (findArticle.author == req.id) {
+        next();
+    } else {
+        return res.status(403).json({
+            msg: "Unauthorized Articles"
+        });
+    }
+
+}
