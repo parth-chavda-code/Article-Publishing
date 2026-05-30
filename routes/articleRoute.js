7d2e52701c0ee578a251555bc633c426664ec01f
Parth Chavda <parthchavda.work@gmail.com>
2026-05-30 18:45:44 +0530

    feat: add publish article route

--- routes/articleRoute.js
+++ routes/articleRoute.js
@@ -1,10 +1,15 @@
 import express from "express";
+//Middlewares
 import { authMiddleware } from "../middlewares/auth.js";
-import { createArticle } from "../controllers/articleController.js";
+import { isOwner } from "../middlewares/isOwner.js";
+//Controller
+import { createArticle, publishArticle } from "../controllers/articleController.js";
 
 const articleRouter = express.Router();
 
 //Create Article
 articleRouter.post("/create", authMiddleware, createArticle);
+//Publish Article
+articleRouter.patch("/:id/publish", authMiddleware, isOwner, publishArticle);
 
 export { articleRouter };
