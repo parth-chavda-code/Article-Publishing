68065da68c41a051f32b35231d33abf70786609d
Parth Chavda <parthchavda.work@gmail.com>
2026-05-30 14:07:50 +0530

    feat: add user signup route

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,34 @@
+import { userModel } from "../models/user.js";
+import bcrypt from "bcrypt";
+
 export const userSignupController = async (req, res) => {
-    res.json("I'm in userSignup");
+    const { email, password, firstName, lastName } = req.body;
+    //find user
+    const find = await userModel.findOne({ email });
+    if (find) {
+        return res.status(500).json({
+            Message: "User exist"
+        });
+    }
+
+    //create user
+
+    try {
+        //password hashing
+        const passwordHashing = await bcrypt.hash(password, 10);
+
+        //create user
+        const user = userModel.create({
+            email,
+            password,
+            firstName,
+            lastName
+        });
+        res.json("User Register successfully");
+    } catch (err) {
+        res.status(402).json({
+            Error: err,
+            Message: "Error when creating a user"
+        })
+    }
 };
