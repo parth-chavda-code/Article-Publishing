a6e96dba92387d3197fb356284ce2a358c4a8a9c
Parth Chavda <parthchavda.work@gmail.com>
2026-05-30 18:46:15 +0530

    feat: add publish route handler

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -14,10 +14,30 @@ export const createArticle = async (req, res) => {
         res.json({
             msg: "Article saved as draft"
         });
-        
+
     } catch (err) {
         res.status(404).json({
             msg: "Error when creating an article"
         });
+    }
+}
+
+//Publish Article
+export const publishArticle = async (req, res) => {
+    const articleId = req.params.id;
+    try {
+        const updateResult = await articleModel.findByIdAndUpdate(articleId, {
+            publish_date: Date.now(),
+            status: "published"
+        });
+        res.json({
+            msg: "Update successfully",
+        });
+
+    } catch (err) {
+        return res.status(404).json({
+            msg: "Error when updating"
+        })
+
     }
 }
