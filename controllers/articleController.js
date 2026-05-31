bb92837edc8a1c8e989f83ec203ec0ef3bbbdd20
Parth Chavda <parthchavda.work@gmail.com>
2026-05-31 16:40:50 +0530

    feat: add check published

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -25,6 +25,9 @@ export const createArticle = async (req, res) => {
 //Publish Article
 export const publishArticle = async (req, res) => {
     const articleId = req.params.id;
+    if (req.article.status == "published") {
+        return res.status(400).json({ msg: "Article Already Published" });
+    }
     try {
         const updateResult = await articleModel.findByIdAndUpdate(articleId, {
             publish_date: Date.now(),
@@ -44,15 +47,22 @@ export const publishArticle = async (req, res) => {
 
 //Get All the articles
 export const allArticle = async (req, res) => {
-    const page = req.query.page;
-    const limit = req.query.limit;
-    const skip = (page - 1) * 10;
+    const page = req.query.page || 1;
+    const limit = req.query.limit || 10;
+    const skip = (page - 1) * limit;
 
-    const findAll = await articleModel.find({
-        status: "published"
-    }).skip(skip).limit(limit);// skip first n no. Documents and limit shows n no. documents after skip
+    try {
+        const findAll = await articleModel.find({
+            status: "published"
+        }).skip(skip).limit(limit);// skip first n no. Documents and limit shows n no. documents after skip
 
-    res.json({
-        findAll
-    });
+        res.json({
+            findAll
+        });
+
+    } catch (err) {
+        res.status(404).json({
+            msg: "Error in get article"
+        });
+    }
 }
