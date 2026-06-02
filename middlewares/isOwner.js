8221bc4c07166f4478c5dd9164902d11e88936c0
Parth Chavda <parthchavda.work@gmail.com>
2026-06-02 12:47:04 +0530

    feat: add req.article

--- middlewares/isOwner.js
+++ middlewares/isOwner.js
@@ -14,6 +14,7 @@ export async function isOwner(req, res, next) {
     }
 
     if (findArticle.author == req.id) {
+        req.article = findArticle;
         next();
     } else {
         return res.status(403).json({
